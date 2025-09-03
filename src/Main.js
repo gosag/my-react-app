c419aa55b7f0139ba45f1f26142441ca9322a09e
Gosa Girma <gosagirma441@gmail.com>
2025-09-03 14:45:18 +0300

    Added Card.js and Card.css, modified Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,25 @@
 import "./Main.css";
+import data from "./data"
+import Card from "./Card"
+import Header from "./Header"
 export default function Main(){
-  return(
-    <div>
-    
-    </div>
-  )
+  const cards=data.map(items=>{
+    return(
+      <Card
+      image={items.image}
+      country={items.country}
+      place={items.place}
+      date={items.date}
+      description={items.description}
+      maps={items.maps}
+      />
+    )
+  })
+return (
+<div>
+  <Header/> 
+  {cards}
+  
+</div>
+)
 }
